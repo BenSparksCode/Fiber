7ed3a464121f442404ad3f0387175e0e0bf92be8
Ben Sparks <bensparks0@gmail.com>
2021-01-23 14:59:35 +0200

    Added Tx dummy data

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -2,6 +2,41 @@ import React, { Component, createContext } from 'react'
 
 export const AppContext = createContext()
 
+const dummyTxs = [
+    {
+        txID: "0x305a83574cb8e4c51acf6db9fd38ec39e6ef73ffe25cbf1845e8d8f68a5f1696",
+        blockNum: 11711726,
+        date: new Date(),
+        amountUSD: 14258,
+        tokenBorrowed: "WBTC",
+        from: "0x87245c288fcC858BF7225Dc3Ab97D0aD94730757",
+        providers: ["AAVE"],
+        interactions: [
+            {entity: "WBTC"},
+            {entity: "UNISWAP"},
+            {entity: "USDC"},
+            {entity: "aWBTC"},
+            {entity: "aUSDC"},
+        ]
+    },
+    {
+        txID: "0x305a83574cb8e4c51acf6db9fd38ec39e6ef73ffe25cbf1845e8d8f68a5f1696",
+        blockNum: 11711726,
+        date: new Date(),
+        amountUSD: 9000000,
+        tokenBorrowed: "ETH",
+        from: "0x87245c288fcC858BF7225Dc3Ab97D0aD94730757",
+        providers: ["AAVE"],
+        interactions: [
+            {entity: "ETH"},
+            {entity: "UNISWAP"},
+            {entity: "USDC"},
+            {entity: "aETH"},
+            {entity: "aUSDC"},
+        ]
+    }
+]
+
 class AppContextProvider extends Component {
     state = {
         FLs: [],
