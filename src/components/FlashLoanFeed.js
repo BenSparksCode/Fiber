6c5d9da950645b6f39b716f9c9805ec2c2e03513
Ben Sparks <bensparks0@gmail.com>
2021-01-24 09:06:23 +0200

    Added FlashLoan Card component skeleton

--- src/components/FlashLoanFeed.js
+++ src/components/FlashLoanFeed.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { Table } from 'antd';
 
 import { ContentPanel } from './StyledComponents'
+import { FlashLoanListItem } from './FlashLoanListItem'
 
 
 export const FlashLoanFeed = () => {
-
-
-
 
     return (
         <div>
             <ContentPanel>
                 <h1>Flash Loans</h1>
-                <Table />
+                <FlashLoanListItem />
             </ContentPanel>
         </div>
     )
--- src/components/FlashLoanListItem.js
+++ src/components/FlashLoanListItem.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export const FlashLoanListItem = () => {
     return (
-        <div>
-            
+        <div className='FlashLoanListItem'>
+            <div className='Third Third1'>
+                LEFT
+            </div>
+            <div className='Third Third2'>
+                MIDDLE
+            </div>
+            <div className='Third Third3'>
+                RIGHT
+            </div>
         </div>
     )
 }
