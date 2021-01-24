7369ae6c205ff9e8cbaf3c0ba2fa2649280a5ab8
calummurphy <56669700+calummurphy@users.noreply.github.com>
2021-01-24 17:51:07 +0200

    Updated Flash Loans UI v2

--- src/components/FlashLoanListItem.js
+++ src/components/FlashLoanListItem.js
@@ -18,24 +18,21 @@ export const FlashLoanListItem = (props) => {
 
             <div className='Third Third2'>
                 
-                    <div className = 'two Interactions'>Interactions: [WBTC,UNISWAP USDC aWBTC aUSDC  </div>
-                    {/* <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png" height="32" width="32" alt="ETH"></img>
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"></img> */}
-
-                    <div className = 'two LOGOS'> 
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png" height="32" width="32" alt="WBTC"></img> <i class="triangle"></i>
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png" height="32" width="32" alt="UNI"></img> <i class="arrow right"></i>
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png" height="32" width="32" alt="USDC"></img> <i class="arrow right"></i>
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/6038.png" height="32" width="32" alt="AWBTC"></img> <i class="arrow right"></i>
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/5760.png" height="32" width="32" alt="AUSDC"></img>
-                    </div>                  
-               
-
+                <div className = 'two Interactions'>Interactions: [WBTC,UNISWAP USDC aWBTC aUSDC  </div>
+                {/* <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png" height="32" width="32" alt="ETH"></img>
+                <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"></img> */}
+                <div className = 'three LOGOS1'> <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png" height="32" width="32" alt="WBTC"></img> </div> 
+                <div className = 'three LOGOS2'> <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png" height="32" width="32" alt="UNI"></img> </div> 
+                <div className = 'three LOGOS3'> <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png" height="32" width="32" alt="USDC"></img> </div> 
+                <div className = 'three LOGOS4'> <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/6038.png" height="32" width="32" alt="AWBTC"></img> </div> 
+                <div className = 'three LOGOS5'> <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/5760.png" height="32" width="32" alt="AUSDC"></img></div> 
+                                    
+                              
             </div>
 
-            <div className='Third Third3'>
-            <div className = 'three TxHash'>Tx Hash Link: "0x305a83574cb8e4c51acf6db9fd38ec39e6ef73ffe25cbf1845e8d8f68a5f1696"</div>
-            <div className = 'three Address'>FROM Address: "0x87245c288fcC858BF7225Dc3Ab97D0aD94730757" </div>
+                <div className='Third Third3'>
+                <div className = 'four TxHash'>Tx Hash Link: "0x305a83574cb8e4c51acf6db9fd38ec39e6ef73ffe25cbf1845e8d8f68a5f1696"</div>
+                <div className = 'four Address'>FROM Address: "0x87245c288fcC858BF7225Dc3Ab97D0aD94730757" </div>
 
             </div>
 
