33e7f8ef0b2016c23793f1847519c7dccbb43b72
calummurphy <56669700+calummurphy@users.noreply.github.com>
2021-01-25 17:52:38 +0200

    Search Bar UI update

--- src/components/SearchAndFilterControls.js
+++ src/components/SearchAndFilterControls.js
@@ -1,15 +1,61 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
 import { AppContext } from '../contexts/AppContext'
-
+import { FlashLoanListItem } from './FlashLoanListItem'
 import { ContentPanel } from './StyledComponents'
 
-export const SearchAndFilterControls = (props) => {
+import { Menu, Dropdown, Button, message, Space } from 'antd';
+import { DownOutlined} from '@ant-design/icons';
+
+function handleButtonClick(e) {
+  message.info('Click on left button.');
+  console.log('click left button', e);
+}
+
+function handleMenuClick(e) {
+  message.info('Click on menu item.');
+  console.log('click', e);
+}
+
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1" >
+      Block Number
+    </Menu.Item>
+    <Menu.Item key="2" >
+      From Address
+    </Menu.Item>
+    <Menu.Item key="3" >
+      Tx Hash
+    </Menu.Item>
+  </Menu>
+);
+
+
+
+export const SearchAndFilterControls = () => {
+
+    const { FLs } = useContext(AppContext)
+    
     return (
+        <div>
         <ContentPanel>
 
             <h1 className='FLTableTitle'>Search and Filter Controls</h1>
+             <div>
+            <Space wrap>                    
+                <Dropdown overlay={menu}>
+                <Button>
+                    All Filters <DownOutlined />
+                </Button>
+                </Dropdown>
+            </Space>
+
+            <input type="text" placeholder = "Search..."/>
+                
+            </div>
 
         </ContentPanel>
+        </div>
     )
 }
+
