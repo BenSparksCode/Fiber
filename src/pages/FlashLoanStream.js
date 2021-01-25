43347f58892e1711406b1d6ad6b424b0670d6e94
Ben Sparks <bensparks0@gmail.com>
2021-01-25 17:33:58 +0200

    Refactored web3 websocket connections to data engine

--- src/pages/FlashLoanStream.js
+++ src/pages/FlashLoanStream.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 
+import web3 from '../data/Web3Connection'
+
 // const Web3 = require('web3')
 // console.log(Web3.utils.soliditySha3("flashLoan(address,address[],uint256[],uint256[],address,bytes,uint16)"));
 
@@ -8,18 +10,11 @@ const API_KEY = process.env.REACT_APP_ALCHEMY_API
 
 export const FlashLoanStream = () => {
 
-    const web3 = useRef(null)
+    // const web3 = useRef(null)
     const subscription = useRef(null)
 
     const [blockNum, setBlockNum] = useState(-1)
     const [eventData, setEventData] = useState("Nothing yet...")
-
-    useEffect(() => {
-
-        web3.current = createAlchemyWeb3(
-            "wss://eth-mainnet.ws.alchemyapi.io/ws/" + API_KEY,
-        );
-    }, [])
 
     const getTxData = (txHash) => {
         web3.current.eth.getTransaction(txHash, (err, res) => {
@@ -31,46 +26,13 @@ export const FlashLoanStream = () => {
         })
     }
 
-
     const handleSubscribe = () => {
-        console.log("Subscribing...");
-
-        subscription.current = web3.current.eth.subscribe('logs', {
-            address: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
-            fromBlock: 11713028,
-            toBlock: 11713030,
-        }, function (error, result) {
-            if (!error) {
-                console.log(result);
-
-                setBlockNum(result.blockNumber)
-
-                getTxData(result.transactionHash)
-
-                setEventData(JSON.stringify(result, null, 2))
-            }
-
-        })
-            .on("data", function (data) {
-                console.log("NEW DATA:", data);
-            })
-            .on("changed", function (log) {
-                console.log("Subscription changed...");
-            })
-            .on("connected", function (log) {
-                console.log("CONNECTED!!!");
-            })
-
+        subscription.current = web3.subscribeToLogs({address: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'}, ()=>{})
     }
 
     const handleUnsubscribe = () => {
-        subscription.current.unsubscribe(function (error, success) {
-            if (success)
-                console.log('Successfully UN-subscribed!');
-        })
+        web3.unsubscribeFromSub(subscription.current)
     }
-
-
 
     return (
         <div>
