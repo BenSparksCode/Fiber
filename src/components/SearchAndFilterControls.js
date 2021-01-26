3b12959266bfa108a451b8176e315e2fab61761e
Ben Sparks <bensparks0@gmail.com>
2021-01-26 09:22:58 +0200

    Cleaned up Search component - removed redundant code

--- src/components/SearchAndFilterControls.js
+++ src/components/SearchAndFilterControls.js
@@ -1,82 +1,65 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../contexts/AppContext'
-import { FlashLoanListItem } from './FlashLoanListItem'
 import { ContentPanel } from './StyledComponents'
 
-import { Menu, Dropdown, Button, message, Space, Input} from 'antd';
-import { DownOutlined,AudioOutlined } from '@ant-design/icons';
-
-const { Search } = Input;
-
-const suffix = (
-  <AudioOutlined
-    style={{
-      fontSize: 16,
-      color: '#1890ff',
-    }}
-  />
-);
-
-const onSearch = value => console.log(value);
-
-
-
-function handleButtonClick(e) {
-  message.info('Click on left button.');
-  console.log('click left button', e);
-}
-
-function handleMenuClick(e) {
-  message.info('Click on menu item.');
-  console.log('click', e);
-}
-
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1" >
-      Block Number
-    </Menu.Item>
-    <Menu.Item key="2" >
-      From Address
-    </Menu.Item>
-    <Menu.Item key="3" >
-      Tx Hash
-    </Menu.Item>
-  </Menu>
-);
-
+import { Menu, Dropdown, Button, message, Space, Input } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 
 export const SearchAndFilterControls = () => {
 
+  const { Search } = Input;
+
+  function handleButtonClick(e) {
+    message.info('Click on left button.');
+    console.log('click left button', e);
+  }
+
+  function handleMenuClick(e) {
+    message.info('Click on menu item.');
+    console.log('click', e);
+  }
+
+  const onSearch = (value) => {
+    // TODO - search logic here
+    console.log(value);
+  }
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="1" >
+        Block Number
+    </Menu.Item>
+      <Menu.Item key="2" >
+        Sender Address
+    </Menu.Item>
+      <Menu.Item key="3" >
+        Tx Hash
+    </Menu.Item>
+    </Menu>
+  );
+
   return (
-    <div>
-      <ContentPanel>
-
-        <h1 className='FLTableTitle'>Search and Filter Controls</h1>
-
-        <div className='search'>
-          <div className=' filterBar'>
-            <Space wrap>
-              <Dropdown overlay={menu}>
-                <Button>
-                  All Filters
+    <ContentPanel>
+      <div className='search'>
+        <div className=' filterBar'>
+          <Space wrap>
+            <Dropdown overlay={menu}>
+              <Button>
+                All Filters
                   <DownOutlined />
-                </Button>
-              </Dropdown>
-            </Space>
-          </div>
-
-          <div className='searchBar'>
-            <Space direction="vertical">
-              <Search placeholder="search by Block Number / Address / Txn Hash" onSearch={onSearch} enterButton />
-            </Space>
-          </div>
+              </Button>
+            </Dropdown>
+          </Space>
         </div>
 
-
-      </ContentPanel>
-    </div>
+        <div className='searchBar'>
+          <Space direction="vertical">
+            <Search placeholder="Search by Block Number / Address / Txn Hash" onSearch={onSearch} enterButton />
+          </Space>
+        </div>
+      </div>
+    </ContentPanel>
   )
 }
 
