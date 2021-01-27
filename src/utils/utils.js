9c25b210929c4eb11bc846d03b3728903e340323
Ben Sparks <bensparks0@gmail.com>
2021-01-27 13:30:50 +0200

    Split date helper function into different Date and Time functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ const currency = require('currency.js')
 
 export const shortenHash = (hash) => {
     if (!hash) return null
-    return hash.substring(0, 6) + " . . . " + hash.substring(hash.length - 6)
+    return hash.substring(0, 6) + "..." + hash.substring(hash.length - 6)
 }
 
 export const formatBlockNum = (blockNum) => {
@@ -15,13 +15,19 @@ export const currencyFormat = (amount) => {
     return currency(amount, { symbol: "$", separator: " ", precision: 0 }).format()
 }
 
-export const shortenDate = (date) => {
+export const getCardDate = (date) => {
     if (!date) return null
     const options = {
-        year: 'numeric', month: 'numeric', day: 'numeric',
+        year: 'numeric', month: 'short', day: 'numeric',
+    }
+    return Intl.DateTimeFormat('en-GB', options).format(date)
+} 
+
+export const getCardTime = (date) => {
+    if (!date) return null
+    const options = {
         hour: 'numeric', minute: 'numeric',
         hour12: false,
     }
-    return Intl.DateTimeFormat(navigator?.languages[0] ?? 'en-US', options).format(date)
-}
-
+    return Intl.DateTimeFormat(navigator?.languages[0] ?? 'en-GB', options).format(date)
+} 
