30c7829fdf165aa15d6cd7c354c4a6c9f1ed3f75
Ben Sparks <bensparks0@gmail.com>
2021-01-28 20:32:14 +0200

    Added Ethers.js for TX analysis

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -57,24 +57,24 @@ class AppContextProvider extends Component {
         selectedFL: null
     }
 
-    // componentDidMount(){
-    //     console.log("Context mounted");
-    //     // Set up newBlockListener
-    //     const sub = web3.subscribeToNewBlocks({}, (err, res) => {
-    //         if(err) return
-    //         this.setState({
-    //             connectedToMainnet: true,
-    //             latestBlockNum: res.number
-    //         })
-    //     })
-    //     // Set up FL event listeners
-    //     const eventSubs = web3.subscribeToFLLogs()
-    //     // Save subs to state for unsubbing later
-    //     this.setState({
-    //         newBlocksSub: sub,
-    //         FLEventSubs: eventSubs
-    //     })
-    // }
+    componentDidMount(){
+        console.log("Context mounted");
+        // Set up newBlockListener
+        const sub = web3.subscribeToNewBlocks({}, (err, res) => {
+            if(err) return
+            this.setState({
+                connectedToMainnet: true,
+                latestBlockNum: res.number
+            })
+        })
+        // Set up FL event listeners
+        const eventSubs = web3.subscribeToFLLogs()
+        // Save subs to state for unsubbing later
+        this.setState({
+            newBlocksSub: sub,
+            FLEventSubs: eventSubs
+        })
+    }
 
     componentDidUpdate(prevProps, prevState) {
         if(prevState.latestBlockNum != this.state.latestBlockNum){
