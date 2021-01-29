463f2bdcb734fd6f51a2b06ada05a8f6fe956703
Ben Sparks <bensparks0@gmail.com>
2021-01-29 15:35:53 +0200

    Added dynamic FL Feed height calc on render

--- src/components/FlashLoanFeed.js
+++ src/components/FlashLoanFeed.js
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react'
-import { Table } from 'antd';
+import React, { useContext, useEffect, useState } from 'react'
 
 import { AppContext } from '../contexts/AppContext'
 
@@ -8,15 +7,38 @@ import { FlashLoanListItem } from './FlashLoanListItem'
 
 
 export const FlashLoanFeed = () => {
-
     const { FLs } = useContext(AppContext)
+    const [FLFeedContainerHeight, setFLFeedContainerHeight] = useState(300)
+
+    useEffect(() => {
+        const height = window.innerHeight
+            || document.documentElement.clientHeight
+            || document.body.clientHeight
+
+        const OFFSET = 450
+
+        console.log(height, height-OFFSET);
+
+        setFLFeedContainerHeight(height-OFFSET)
+
+    }, [])
+
+
+
+
 
     return (
-            <ContentPanel>
+        <ContentPanel className='FLFeedContainer'
+            style={{
+                height: FLFeedContainerHeight,
+                minHeight: FLFeedContainerHeight,
+                maxHeight: FLFeedContainerHeight,
+            }}
+        >
 
-                {FLs.map(FL => <FlashLoanListItem data={FL}/>)}
+            {FLs.map(FL => <FlashLoanListItem data={FL} />)}
 
-            </ContentPanel>
+        </ContentPanel>
     )
 }
 
