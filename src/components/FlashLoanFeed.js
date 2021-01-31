da536cb3f0573fa91b8e69db6f013a94d66d06c5
Ben Sparks <bensparks0@gmail.com>
2021-02-01 00:03:31 +0200

    Removed algorithmic height calcs in React - height scaling solved with CSS for now

--- src/components/FlashLoanFeed.js
+++ src/components/FlashLoanFeed.js
@@ -5,32 +5,11 @@ import { AppContext } from '../contexts/AppContext'
 import { ContentPanel } from './StyledComponents'
 import { FlashLoanListItem } from './FlashLoanListItem'
 
-
 export const FlashLoanFeed = () => {
     const { FLs } = useContext(AppContext)
-    const [FLFeedContainerHeight, setFLFeedContainerHeight] = useState(300)
-
-    useEffect(() => {
-        const height = window.innerHeight
-            || document.documentElement.clientHeight
-            || document.body.clientHeight
-
-        const OFFSET = 450
-
-        console.log(height, height-OFFSET);
-
-        setFLFeedContainerHeight(height-OFFSET)
-
-    }, [])
 
     return (
-        <ContentPanel className='FLFeedContainer'
-            // style={{
-            //     height: FLFeedContainerHeight,
-            //     minHeight: FLFeedContainerHeight,
-            //     maxHeight: FLFeedContainerHeight,
-            // }}
-        >
+        <ContentPanel className='FLFeedContainer'>
 
             {FLs.map(FL => <FlashLoanListItem data={FL} />)}
 
