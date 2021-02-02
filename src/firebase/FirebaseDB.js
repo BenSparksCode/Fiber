b2267fcb6d318df818b922d3af9de17ad940de4f
Ben Sparks <bensparks0@gmail.com>
2021-02-02 10:44:02 +0200

    Made function to transfer items between Firebase collections

--- src/firebase/FirebaseDB.js
+++ src/firebase/FirebaseDB.js
@@ -9,14 +9,16 @@ class FirebaseDB {
             console.log("ERROR in FIREBASE DB: Missing data in storeFlashLoan()")
             return null
         }
-        const collectionRef = await auth.db.collection('flashLoans')
+        const collectionRef = await auth.db.collection('flashLoans3')
         try {
             const res = await collectionRef.add({
-                txHash: data.tx?.hash,
+                txHash: data.txHash,
+                from: data.from,
                 block: data.block,
                 version: data.version,
                 tx: JSON.stringify(data.tx),
                 decodedTX: JSON.stringify(data.decodedTX),
+                borrowData: JSON.stringify(data.borrowData),
                 events: JSON.stringify(data.events),
                 dateCreated: firebase.firestore.FieldValue.serverTimestamp(),
             })
@@ -28,16 +30,16 @@ class FirebaseDB {
     }
 
     async getAllFlashLoans() {
-        console.log("in getAllFlashLoans");
         if (!auth.isUserSignedIn()) return null
         let FLs = []
-        const colRef = auth.db.collection('flashLoans')
+        const colRef = auth.db.collection('flashLoans3')
 
-        return colRef.get()
+        return colRef
+            .orderBy('dateCreated', 'desc').limit(10)
+            .get()
             .then(snapshot => {
                 snapshot.forEach(doc => {
                     const flObj = { ...doc.data(), ...{ id: doc.id } }
-                    console.log(flObj);
                     FLs.push(flObj)
                 })
             })
@@ -48,6 +50,43 @@ class FirebaseDB {
                 console.log("ERROR in FIREBASE DB: Error in getAllFlashLoans", err);
             })
     }
+
+
+    // USE FUNCTION BELOW TO TRANSFER ITEMS BETWEEN FIREBASE COLLECTIONS
+
+    // async moveFLToNewCollection() {
+    //     const from = 'flashLoans3'
+    //     const to = 'flashLoans'
+
+    //     if (!auth.isUserSignedIn()) return null
+    //     let FLs = []
+    //     const colRef = auth.db.collection(from)
+
+    //     return colRef
+    //         .get()
+    //         .then(snapshot => {
+    //             snapshot.forEach(doc => {
+    //                 const flObj = { ...doc.data() }
+    //                 FLs.push(flObj)
+    //             })
+    //         })
+    //         .then(async () => {
+    //             const collectionRef = await auth.db.collection(to)
+    //             try {
+    //                 for (let i = 0; i < FLs.length; i++) {
+    //                     collectionRef.add(FLs[i])
+    //                 }
+    //                 console.log("SUCCESS: Moved all items from "+from+" to "+to);
+    //                 return true
+    //             } catch (err) {
+    //                 console.log("ERROR in FIREBASE DB: Error in moveFLToNewCollection", err);
+    //                 return null
+    //             }
+    //         })
+    //         .catch(err => {
+    //             console.log("ERROR in FIREBASE DB: Error in moveFLToNewCollection", err);
+    //         })
+    // }
 }
 
 export default new FirebaseDB()
